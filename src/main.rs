c202645a4a1c3feb27359c5873264ac6751e968c
HangBeni <hangbenjamin2003@gmail.com>
2024-03-19 17:28:46 +0100

    feat: organized mod import
    I moved the app and ui modules to top. So it can be better readed.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-use app::App;
-use crossterm::{event::{self, EnableMouseCapture, Event}, execute, terminal::{enable_raw_mode, EnterAlternateScreen}};
-use ratatui::{backend::{Backend, CrosstermBackend}, terminal, Terminal};
 mod app;
 mod ui;
+use crossterm::{
+    event::{self, EnableMouseCapture, Event},
+    execute,
+    terminal::{enable_raw_mode, EnterAlternateScreen},
+};
+use ratatui::{
+    backend::{Backend, CrosstermBackend},
+    terminal, Terminal,
+};
+
+use crate::{
+    app::{App, Course, CurrentScreen},
+    ui::ui,
+};
 use std::{error::Error, io};
-fn main() -> Result<(), Box<dyn Error>>{
+
+fn main() -> Result<(), Box<dyn Error>> {
     //terminal setup
     enable_raw_mode()?;
     let mut stdout = io::stdout();
@@ -15,20 +27,22 @@ fn main() -> Result<(), Box<dyn Error>>{
     let mut app = App::new();
     let res = run_app(&mut terminal, &mut app);
 
-   Ok(())
+    Ok(())
 }
 
 //Az app folyamatát kezeli (lap váltás stb.)
-fn run_app<B:Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<bool> {
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<bool> {
     loop {
         terminal.draw(|f| ui(f, app))?;
         if let Event::Key(key) = event::read()? {
-            if key.kind == event::KeyEventKind::Release{continue;}
-        match app.current_screen{
-            app::CurrentScreen::Home => {
-                
-                }
-            _ => {}
+            if key.kind == event::KeyEventKind::Release {
+                continue;
+            }
+            match app.current_screen {
+                app::CurrentScreen::Home => {}
+                app::CurrentScreen::Course => {}
+                app::CurrentScreen::TimeTable => {}
+
             }
         }
     }
