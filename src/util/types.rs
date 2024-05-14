ac615bf821f79b53202d0ab49bf14e04771c545d
HangBeni <hangbenjamin2003@gmail.com>
2024-05-14 10:53:04 +0200

    feat: base types

--- src/util/types.rs
+++ src/util/types.rs
@@ -1,20 +1,41 @@
+use strum::Display;
+
 // az enum értékekhez lehet string értéket is társítani -- cargo add strum --features derive,strum_macros
 //  #[strum(serialize = "érték")]
 
+use std::io;
+
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+#[derive(Serialize, Deserialize,Clone, Display)]
 pub enum Lecture {
+    #[strum(serialize = "Előadás")]
     Theory,
+    #[strum(serialize = "Gyakorlat")]
     Practical,
+    #[strum(serialize = "Labor")]
     Lab,
 }
+#[derive(Serialize, Deserialize,Clone)]
 pub enum Role {
     Nebulo,
     Teacher,
 }
+#[derive(Copy,Clone,Debug)]
 pub enum CurrentScreen {
     Home,
     Courses,
     TimeTable,
     Exiting
+}
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("error reading the DB file: {0}")]
+    ReadDBError(#[from] io::Error),
+    #[error("error parsing the DB file: {0}")]
+    ParseDBError(#[from] serde_json::Error),
 }
 
 impl From<CurrentScreen> for usize {
@@ -27,20 +48,20 @@ impl From<CurrentScreen> for usize {
         }
     }
 }
-
+#[derive(Serialize, Deserialize,Clone)]
 pub struct Course {
-    id: usize,
-    code: String, // egyedi azon. név+type
-    lecture_type: Lecture,
-    name: String,
-    length: String,
+   pub id: usize,
+   pub code: String, // egyedi azon. név+type
+   pub lecture_type: Lecture,
+   pub name: String,
+   pub length: String,
 }
-
+#[derive(Serialize, Deserialize,Clone)]
 pub struct User {
-    id: usize,
-    code: [char; 6],  //Neptun
-    password: String, // jó lenne hashelve
-    name: String,
-    role: Role,
-    user_schedule: Vec<String>, //az órák kódját kell eltárolni hogy ha a kurzus változik akkor változzon a hivatkozással
+   pub id: usize,
+   pub code: [char; 6],  //Neptun
+   pub password: String, // jó lenne hashelve
+   pub name: String,
+   pub role: Role,
+   pub user_schedule: Vec<String>, //az órák kódját kell eltárolni hogy ha a kurzus változik akkor változzon a hivatkozással
 }
