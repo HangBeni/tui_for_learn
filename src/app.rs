93ad8bc8e457d8fadd275a7373bdd23088274569
HangBeni <hangbenjamin2003@gmail.com>
2024-05-15 18:28:29 +0200

    feat: login input highlight, validation mechanic base

--- src/app.rs
+++ src/app.rs
@@ -1,17 +1,21 @@
-use tui_for_learn::util::types::{Course, CurrentScreen};
+use tui_for_learn::util::types::{CurrentScreen, LoginHighlights, LoginValidation};
 
-
-
-pub struct App{
+pub struct App {
     pub current_screen: CurrentScreen,
+    pub current_login_parameter: LoginHighlights,
+    pub code_input: String,
+    pub password_input: String,
 }
 
 impl App {
-
     pub fn new() -> App {
-        App{ 
-            current_screen: CurrentScreen::Home,
+        App {
+            current_screen: CurrentScreen::Login,
+            current_login_parameter: LoginHighlights::Neptun {
+                valid: LoginValidation::Pending,
+            },
+            code_input: String::new(),
+            password_input: String::new(),
         }
     }
 }
-
