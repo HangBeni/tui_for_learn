3b050058f5318eddb97a8e2b22e002633e4b6451
HangBeni <hangbenjamin2003@gmail.com>
2024-05-16 20:14:30 +0200

    feat: login status struct

--- src/util/types.rs
+++ src/util/types.rs
@@ -8,7 +8,7 @@ use std::io;
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
-#[derive(Serialize, Deserialize,Clone, Display)]
+#[derive(Serialize, Deserialize, Clone, Display)]
 pub enum Lecture {
     #[strum(serialize = "Eloadás")]
     Theory,
@@ -17,32 +17,52 @@ pub enum Lecture {
     #[strum(serialize = "Labor")]
     Lab,
 }
-#[derive(Serialize, Deserialize,Clone)]
+#[derive(Serialize, Deserialize, Clone)]
 pub enum Role {
     Nebulo,
     Teacher,
 }
-#[derive(Copy,Clone,Debug)]
+#[derive(Copy, Clone, Debug)]
 pub enum CurrentScreen {
     Home,
     Courses,
     TimeTable,
     Exiting,
-    Login
+    Login,
 }
 
-#[derive(Copy,Clone,Debug)]
-pub enum LoginHighlights{
-    Neptun{valid:LoginValidation},
-    Password{valid:LoginValidation},
-    None
+impl From<CurrentScreen> for usize {
+    fn from(value: CurrentScreen) -> usize {
+        match value {
+            CurrentScreen::Login => 0,
+            CurrentScreen::Home => 1,
+            CurrentScreen::Courses => 2,
+            CurrentScreen::TimeTable => 3,
+            CurrentScreen::Exiting => 4,
+        }
+    }
 }
 
-#[derive(Copy,Clone,Debug)]
-pub enum LoginValidation {
-    Valid,
-    NotValid,
-    Pending
+#[derive(Copy, Clone, Debug)]
+pub enum LoginHighlight {
+    Neptun,
+    Password,
+    None,
+}
+
+pub struct LoginState{
+   pub neptun:String, //Display if the check is not passed through
+   pub password:String, //Display if the check is not passed through
+   pub user: Option<User> // The logged user
+}
+impl LoginState {
+    pub fn initialize() ->  LoginState {
+        LoginState{
+            neptun:"Neptun Code".to_owned(),
+            password: "Password".to_owned(),
+            user: None
+        }
+    }
 }
 
 #[derive(Error, Debug)]
@@ -53,34 +73,23 @@ pub enum Error {
     ParseDBError(#[from] serde_json::Error),
 }
 
-impl From<CurrentScreen> for usize {
-    fn from(value: CurrentScreen) -> usize {
-        match value {
-            CurrentScreen::Login => 0,
-            CurrentScreen::Home => 1,
-            CurrentScreen::Courses => 2,
-            CurrentScreen::TimeTable => 3,
-            CurrentScreen::Exiting => 4
-        }
-    }
-}
-#[derive(Serialize, Deserialize,Clone)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Course {
-   pub id: usize,
-   pub code: String, // egyedi azon. név+type
-   pub lecture_type: Lecture,
-   pub name: String,
-   pub length: String,
+    pub id: usize,
+    pub code: String, // egyedi azon. név+type
+    pub lecture_type: Lecture,
+    pub name: String,
+    pub length: String,
 }
-#[derive(Serialize, Deserialize,Clone)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct User {
-   pub id: usize,
-   pub code: [char; 6],  //Neptun
-   pub password: String, // jó lenne hashelve
-   pub name: String,
-   pub role: Role,
-   pub uni: String,
-   pub faculty: String,
-   pub major: String,
-   pub user_schedule: Vec<String>, //az órák kódját kell eltárolni hogy ha a kurzus változik akkor változzon a hivatkozással
+    pub id: usize,
+    pub code: String,  //Neptun
+    pub password: String, // jó lenne hashelve
+    pub name: String,
+    pub role: Role,
+    pub uni: String,
+    pub faculty: String,
+    pub major: String,
+    pub user_schedule: Vec<String>, //az órák kódját kell eltárolni hogy ha a kurzus változik akkor változzon a hivatkozással
 }
