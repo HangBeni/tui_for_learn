7906ce33512a557d609dfbf48b7b73f2706fbd3f
HangBeni <hangbenjamin2003@gmail.com>
2024-06-01 17:00:43 +0200

    feat: correct styling

--- src/pages/login.rs
+++ src/pages/login.rs
@@ -5,76 +5,82 @@ use ratatui::{
     Frame,
 };
 
-use crate::{components::c_rect::centered_rect, util::types::{App, LoginHighlight, LoginState}};
+use crate::{
+    components::c_rect::centered_rect,
+    util::types::{App, LoginHighlight, LoginState},
+};
 
 pub fn render_login(f: &mut Frame, app: &App, login_state: &mut LoginState) {
     let area = centered_rect(50, 50, f.size());
     let login_chunks = Layout::default()
         .direction(Direction::Vertical)
         .margin(1)
-        .constraints([
-            Constraint::Percentage(15),
-            Constraint::Percentage(15)
-        ])
+        .constraints([Constraint::Length(3), Constraint::Length(3)])
         .split(area);
 
-
-    let default_style = Style::default().bg(Color::LightMagenta).fg(Color::White);
-    let error_style = Style::default().bg(Color::default()).fg(Color::White);
+    let default_style = Style::default().bg(Color::default()).fg(Color::White);
+    let unfocus_style = Style::default().bg(Color::LightMagenta).fg(Color::Gray);
     let passed_style = Style::default().bg(Color::LightGreen).fg(Color::White);
-    let active_style = Style::default().bg(Color::Reset).fg(Color::White);
+    let active_style = Style::default().bg(Color::Cyan).fg(Color::White);
 
     let mut code_block = Block::default()
-        .title(login_state.neptun.clone())   
+        .title(login_state.neptun.clone())
         .borders(Borders::ALL)
-        .border_type(BorderType::Plain);
+        .border_type(BorderType::Rounded);
 
     let mut password_block = Block::default()
         .title(login_state.password.clone())
         .borders(Borders::ALL)
-        .border_type(BorderType::Plain);
+        .border_type(BorderType::Rounded);
 
     match &login_state.user {
         Some(user) => {
-            code_block = code_block.style(passed_style).border_type(BorderType::Plain).title(user.name.clone());
-            password_block = password_block.style(passed_style).border_type(BorderType::Plain);
-        },
-        None => {
-            match app.current_login_parameter {
-                LoginHighlight::Neptun =>{ 
-                    match login_state.neptun.contains("ERROR") {
-                    true => {
-                        code_block = code_block
-                            .style(error_style)
-                            .border_style(Color::Red).border_type(BorderType::Double);
-                    }
-                    false => {
-                        code_block = code_block
-                            .style(active_style)
-                            .border_style(Color::Green).border_type(BorderType::Double);
-                    }
-                }},
-                LoginHighlight::Password => match login_state.password.contains("ERROR") {
+            code_block = code_block
+                .style(passed_style)
+                .border_type(BorderType::Thick)
+                .title(user.name.clone());
+            password_block = password_block
+                .style(passed_style)
+                .border_type(BorderType::Thick);
+        }
+        None => match app.current_login_parameter {
+            LoginHighlight::None => {
+                code_block = code_block.style(unfocus_style);
+                password_block = password_block.style(unfocus_style);
+            }
+            _ => {
+                match login_state.password.contains("ERROR") {
                     true => {
                         password_block = password_block
-                            .style(error_style)
-                            .border_style(Color::Red).border_type(BorderType::Double);
+                            .border_style(Color::Red)
+                            .border_type(BorderType::Plain);
+                        code_block = code_block.style(default_style);
                     }
                     false => {
-                        password_block = password_block
-                            .style(active_style)
-                            .border_style(Color::Green).border_type(BorderType::Double);
+                        if app.current_login_parameter == LoginHighlight::Password {
+                            password_block = password_block.style(active_style).border_type(BorderType::Double);
+                            code_block = code_block.style(default_style);
+                        }
                     }
-                },
-        
-                LoginHighlight::None => {
-                    code_block = code_block.style(default_style);
-                    password_block = password_block.style(default_style);
+                }
+
+                match login_state.neptun.contains("ERROR") {
+                    true => {
+                        code_block = code_block
+                            .border_style(Color::Red)
+                            .border_type(BorderType::Plain);
+                        password_block = password_block.style(default_style);
+                    }
+                    false => {
+                        if app.current_login_parameter == LoginHighlight::Neptun {
+                            code_block = code_block.style(active_style).border_type(BorderType::Double);
+                            password_block = password_block.style(default_style);
+                        }
+                    }
                 }
             }
         },
     }
-   
 
     //Input field
     let code_text = Paragraph::new(app.code_input.clone()).block(code_block);
