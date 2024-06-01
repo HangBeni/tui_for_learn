2191b09a09b8d8a5d8599c010970d705545b2ccc
HangBeni <hangbenjamin2003@gmail.com>
2024-06-01 16:59:51 +0200

    feat: error messaging, highligting, wait for log in bug: passed style doesn't show up

--- src/util/handlers.rs
+++ src/util/handlers.rs
@@ -1,6 +1,3 @@
-use core::time;
-use std::thread;
-
 use crossterm::event::KeyCode;
 
 use super::{
@@ -9,21 +6,25 @@ use super::{
 };
 
 pub fn handle_validation(app: &mut App, login_state: &mut LoginState) {
+
     match app.current_login_parameter {
+
         LoginHighlight::Neptun => {
+
             let result = check_code(&app.code_input);
+
             match result {
                 Ok(res) => {
                     login_state.neptun = res.to_owned();
-                    if app.password_input.len() > 0 {
+                    if app.password_input.len() > 0 && app.code_input.len() == 6 {
+                        
                         login_state.user = logger(&app.code_input, &app.password_input);
 
                         match login_state.user {
                             Some(_) => {
-                                //Vár a program hogy lássuk a jó bejelentkezés színeit
-                                thread::sleep(time::Duration::from_secs(3));
-
-                                app.current_screen = CurrentScreen::Home
+                                app.current_login_parameter = LoginHighlight::Password;
+                                std::thread::sleep(std::time::Duration::from_secs(1));
+                                app.current_screen = CurrentScreen::Home;
                             }
                             None => {
                                 login_state.neptun = "Not Found!".to_owned();
@@ -36,6 +37,7 @@ pub fn handle_validation(app: &mut App, login_state: &mut LoginState) {
                 }
                 Err(err) => {
                     login_state.neptun = err.to_owned();
+                    login_state.password = login_state.password.replace("ERROR", "");
                     app.current_login_parameter = LoginHighlight::Password;
                 }
             }
@@ -43,13 +45,19 @@ pub fn handle_validation(app: &mut App, login_state: &mut LoginState) {
 
         LoginHighlight::Password => {
             let result = check_password(&app.password_input);
+
             match result {
                 Ok(res) => {
                     login_state.password = res.to_owned();
-                    if app.code_input.len() == 6 {
+                    if app.code_input.len() == 6 && app.password_input.len() > 3 {
                         login_state.user = logger(&app.code_input, &app.password_input);
+
                         match login_state.user {
-                            Some(_) => app.current_screen = CurrentScreen::Home,
+                            Some(_) => {
+                                app.current_login_parameter = LoginHighlight::Neptun;
+                                std::thread::sleep(std::time::Duration::from_secs(1));
+                                app.current_screen = CurrentScreen::Home;
+                            }
                             None => {
                                 login_state.neptun = "Not Found!".to_owned();
                                 app.current_login_parameter = LoginHighlight::Neptun;
@@ -61,6 +69,7 @@ pub fn handle_validation(app: &mut App, login_state: &mut LoginState) {
                 }
                 Err(err) => {
                     login_state.password = err.to_owned();
+                    login_state.neptun = login_state.neptun.replace("ERROR", "");
                     app.current_login_parameter = LoginHighlight::Neptun;
                 }
             }
@@ -71,12 +80,9 @@ pub fn handle_validation(app: &mut App, login_state: &mut LoginState) {
 }
 
 pub fn handle_input(char: char, app: &mut App, login_state: &mut LoginState) {
-
     match app.current_login_parameter {
         LoginHighlight::Neptun => {
-            login_state.neptun = "Neptun Code".to_owned();
-
-            app.code_input.push(char);
+            app.code_input.push(char.to_ascii_uppercase());
 
             if app.code_input.len().ge(&6) || !char.is_alphanumeric() {
                 let result = check_code(&app.code_input);
@@ -93,20 +99,8 @@ pub fn handle_input(char: char, app: &mut App, login_state: &mut LoginState) {
         }
 
         LoginHighlight::Password => {
-            login_state.password = "Password".to_owned();
-
+            login_state.password = login_state.password.replace("ERROR", "");
             app.password_input.push(char);
-
-            let result = check_password(&app.password_input);
-
-            match result {
-                Ok(res) => {
-                    login_state.password = res.to_owned();
-                }
-                Err(err) => {
-                    login_state.password = err.to_owned();
-                }
-            }
         }
         LoginHighlight::None => {
             if char == 'q' {
@@ -119,8 +113,6 @@ pub fn handle_input(char: char, app: &mut App, login_state: &mut LoginState) {
 pub fn handle_deletion(app: &mut App, login_state: &mut LoginState) {
     match app.current_login_parameter {
         LoginHighlight::Neptun => {
-            login_state.neptun = "Neptun Code".to_owned();
-
             app.code_input.pop();
 
             let result = check_code(&app.code_input);
@@ -135,8 +127,6 @@ pub fn handle_deletion(app: &mut App, login_state: &mut LoginState) {
             }
         }
         LoginHighlight::Password => {
-            login_state.password = "Password".to_owned();
-
             app.password_input.pop();
 
             let result = check_password(&app.password_input);
