8d4feca64045167862ffb98c8eead5512e95912e
HangBeni <hangbenjamin2003@gmail.com>
2024-06-13 15:23:15 +0200

    created: home bar

---
+++ src/components/status_bars/movement.rs
@@ -0,0 +1,26 @@
+use ratatui::{
+    style::{Color, Style},
+    text::Line,
+    widgets::{Block, Paragraph},
+};
+
+use crate::util::types::{CurrentScreen, User};
+
+pub fn movement_bar<'a>(current_screen: CurrentScreen, user: &User) -> Paragraph<'a> {
+    let status_bar: Line = Line::from(vec![
+        format!("User Code: {}", user.code).into(),
+        format!(" | Navigation: <j/k> <arrow keys> | <1, 2, 3, q> for navigation |").into(),
+        format!(
+            " | Current Time: {}",
+            chrono::Local::now().format("%H:%M:%S %Y-%m-%d")
+        )
+        .into(),
+        format!(" | Current Screen: {:?}", current_screen).into()
+    ]);
+
+    let block = Block::bordered()
+        .style(Style::default().fg(Color::White))
+        .title("Status Bar");
+    let status_bar = Paragraph::new(status_bar).block(block);
+    status_bar
+}
