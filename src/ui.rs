4c8c9ea537de25e5e6dc01b97c70f2b7eba8e42e
HangBeni <hangbenjamin2003@gmail.com>
2024-06-13 15:11:44 +0200

    feat: statusbar for screens

--- src/ui.rs
+++ src/ui.rs
@@ -4,7 +4,9 @@ use ratatui::{
     Frame,
 };
 use tui_for_learn::{
-    components::{c_rect::centered_rect, exit::exit_popup, nav::render_nav, status::status_bar},
+    components::{
+        c_rect::centered_rect, exit::exit_popup, give_up::give_up_popup, nav::render_nav, status_bars::{course_help::courses_bar, movement::movement_bar, timetabel_help::timetable_bar},
+    },
     pages::{
         courses::{render_courses, render_courses_with_taked},
         home::render_home,
@@ -22,38 +24,56 @@ pub fn ui(f: &mut Frame, app: &App, courses: &mut ListState, login_state: &mut L
         .constraints([
             Constraint::Length(3),
             Constraint::Percentage(80),
-            Constraint::Length(2),
+            Constraint::Length(3),
         ])
         .split(f.size());
 
     //Alap layout
     if login_state.user.is_some() {
-        f.render_widget(render_nav(app.current_screen), layout_area[0]);
-        f.render_widget(status_bar(), layout_area[2]);
+        f.render_widget(
+            render_nav(usize::from(app.current_screen) - 1),
+            layout_area[0],
+        );
     }
 
-    //A képernyő kiválasztása
+    //A képernyők
     match app.current_screen {
         CurrentScreen::Login => {
             render_login(f, app, login_state);
         }
         CurrentScreen::Home => {
             f.render_widget(render_home(login_state.user.clone()), layout_area[1]);
+            f.render_widget(
+                movement_bar(app.current_screen, &login_state.user.clone().unwrap()),
+                layout_area[2]
+            );
         }
-        CurrentScreen::Courses => match login_state.user.clone() {
+        CurrentScreen::Courses => {
+            f.render_widget(courses_bar(app.current_screen, &login_state.user.clone().unwrap()), layout_area[2]);
+            match login_state.user.clone() {
             Some(mut user) => match user.user_schedule.len() {
                 0 => render_courses(f, courses, layout_area),
                 _ => {
-                    render_courses_with_taked(f, courses, layout_area, &mut user, app.current_course_list);
+                    render_courses_with_taked(
+                        f,
+                        courses,
+                        layout_area,
+                        &mut user,
+                        app.current_course_list,
+                    );
                 }
             },
             _ => {}
-        },
+        }},
         CurrentScreen::TimeTable => {
             f.render_widget(render_time_table(), layout_area[1]);
+            f.render_widget(timetable_bar(app.current_screen, &login_state.user.clone().unwrap()), layout_area[2]);
         }
         CurrentScreen::Exiting => {
             f.render_widget(exit_popup(), centered_rect(40, 20, f.size()));
+        }
+        CurrentScreen::GiveUpCourse => {
+            f.render_widget(give_up_popup(), centered_rect(40, 20, f.size()));
         }
     }
 }
