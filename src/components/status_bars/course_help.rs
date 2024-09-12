f1dd1849f658ac4623753decc65ad1c37abc27e3
HangBeni <hangbenjamin2003@gmail.com>
2024-09-12 18:53:57 +0200

    ch: user code display

--- src/components/status_bars/course_help.rs
+++ src/components/status_bars/course_help.rs
@@ -8,7 +8,7 @@ use crate::util::types::{CurrentScreen, User};
 
 pub fn courses_bar<'a>(current_screen: CurrentScreen, user: &User) -> Paragraph<'a> {
     let status_bar: Line = Line::from(vec![
-        format!("User Code: {}", user.code).into(),
+        format!("User: {}", user.code).into(),
         format!(" | Navigation: <j/k> <arrow keys> | <enter/a> sign up | <r> remove | <tab> switch | ").into(),
         format!(
             "Current Time: {}",
